a1f6ec9702008fd263d60afae3432a9c6de866e3
Ricky Cruz <rczdev@gmail.com>
2024-04-11 22:50:00 -0600

    Create poster and title widget

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -12,7 +12,12 @@ class DetailScreen extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          _CustomAppBar()
+          _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              _PosterAndTitle()
+            ])
+          )
         ],
       ),
     );
@@ -44,6 +49,57 @@ class _CustomAppBar extends StatelessWidget {
           image: NetworkImage('https://via.placeholder.com/500x300'),
           fit: BoxFit.cover,
         ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'), 
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              height: 150,
+            ),
+          ),
+
+          const SizedBox(width: 20),
+
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'movie.title', 
+                style: textTheme.headlineSmall,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                'movie.originalTitle', 
+                style: textTheme.titleSmall,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Row(
+                children: [
+                  const Icon(Icons.star_outline, size: 18, color: Colors.grey),
+                  const SizedBox(width: 4),
+                  Text('movie.average', style: textTheme.bodySmall)
+                ],
+              )
+            ]
+          )
+        ],
       ),
     );
   }
