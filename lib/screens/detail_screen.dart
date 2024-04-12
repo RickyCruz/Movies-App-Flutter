6e1307a789ca3c741ff3879ce16c538aa1a807f1
Ricky Cruz <rczdev@gmail.com>
2024-04-11 18:13:31 -0600

    Add DetailScreen using Slivers

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -3,14 +3,48 @@ import 'package:flutter/material.dart';
 class DetailScreen extends StatelessWidget {
   const DetailScreen({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        body: Container(
-          child: const Center(
-            child: Text('Detail Screen'),
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: This must be an instance of movie
+    final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar()
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.orange,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        titlePadding: const EdgeInsets.all(0),
+        centerTitle: true,
+        title: Container(
+          color: Colors.black12,
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          child: const Text(
+            'movie.title',
+            style: TextStyle(fontSize: 18),
           ),
         ),
-      );
-    }
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'), 
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
 }
