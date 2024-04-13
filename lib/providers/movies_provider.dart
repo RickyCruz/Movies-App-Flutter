0d9d5243ccfd61a517e0a41df0978f7bea17fea9
Ricky Cruz <rczdev@gmail.com>
2024-04-12 21:57:36 -0600

    Refactor source code

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -12,41 +12,41 @@ class MoviesProvider extends ChangeNotifier {
   List<Movie> onDisplayMovies = [];
   List<Movie> popularMovies = [];
 
+  int _popularPage = 0;
+
   MoviesProvider() {
     getOnDisplayMovies();
     getPopularMovies();
   }
 
-  getOnDisplayMovies() async {
+  Future<String> _getJsonData(String endpoint, [int page = 1]) async {
     var url = Uri.https(
-      _baseUrl,
-      '3/movie/now_playing', 
+      _baseUrl, 
+      endpoint,
       {
         'api_key': _apiKey,
         'language': _language,
-        'page': '1'
+        'page': '$page'
       }
     );
-
     final response = await http.get(url);
-    final nowPlaying = NowPlayingResponse.fromRawJson(response.body);
+
+    return response.body;
+  }
+
+  getOnDisplayMovies() async {
+    final nowPlaying = NowPlayingResponse.fromRawJson(
+      await _getJsonData('3/movie/now_playing')
+    );
     onDisplayMovies = nowPlaying.results;
     notifyListeners();
   }
 
   getPopularMovies() async {
-    var url = Uri.https(
-      _baseUrl,
-      '3/movie/popular', 
-      {
-        'api_key': _apiKey,
-        'language': _language,
-        'page': '1'
-      }
+    _popularPage++;
+    final popular = PopularResponse.fromRawJson(
+      await _getJsonData('3/movie/popular', _popularPage)
     );
-
-    final response = await http.get(url);
-    final popular = PopularResponse.fromRawJson(response.body);
     popularMovies = [...popularMovies, ...popular.results];
     notifyListeners();
   }
