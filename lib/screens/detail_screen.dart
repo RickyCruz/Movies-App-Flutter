5f536be69213650ccefe5510e05f677daf6c8583
Ricky Cruz <rczdev@gmail.com>
2024-05-30 00:40:10 -0600

    Add animations to text

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:movies_app/models/movie.dart';
 import 'package:movies_app/widgets/casting_cards.dart';
@@ -46,10 +47,13 @@ class _CustomAppBar extends StatelessWidget {
           width: double.infinity,
           alignment: Alignment.bottomCenter,
           padding: const EdgeInsets.only(bottom: 10, left: 10, right: 10),
-          child: Text(
-            movie.title,
-            style: const TextStyle(fontSize: 16),
-            textAlign: TextAlign.center,
+          child: FadeIn(
+            delay: Durations.medium2,
+            child: Text(
+              movie.title,
+              style: const TextStyle(fontSize: 16),
+              textAlign: TextAlign.center,
+            ),
           ),
         ),
         background: FadeInImage(
@@ -97,24 +101,33 @@ class _PosterAndTitle extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  movie.title,
-                  style: textTheme.headlineSmall,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 2,
+                FadeIn(
+                  delay: Durations.medium2,
+                  child: Text(
+                    movie.title,
+                    style: textTheme.headlineSmall,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                  ),
                 ),
-                Text(
-                  movie.originalTitle, 
-                  style: textTheme.titleSmall,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 2,
+                FadeIn(
+                  delay: Durations.long2,
+                  child: Text(
+                    movie.originalTitle,
+                    style: textTheme.titleSmall,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                  ),
                 ),
-                Row(
-                  children: [
-                    const Icon(Icons.star_outline, size: 18, color: Colors.grey),
-                    const SizedBox(width: 4),
-                    Text('${movie.voteAverage}', style: textTheme.bodySmall)
-                  ],
+                FadeIn(
+                  delay: Durations.extralong1,
+                  child: Row(
+                    children: [
+                      const Icon(Icons.star_outline, size: 18, color: Colors.grey),
+                      const SizedBox(width: 4),
+                      Text('${movie.voteAverage}', style: textTheme.bodySmall)
+                    ],
+                  ),
                 )
               ]
             ),
@@ -134,10 +147,13 @@ class _Overview extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      child: Text(
-        movie.overview,
-        textAlign: TextAlign.justify,
-        style: Theme.of(context).textTheme.titleMedium,
+      child: FadeIn(
+        delay: Durations.extralong2,
+        child: Text(
+          movie.overview,
+          textAlign: TextAlign.justify,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
       ),
     );
   }
